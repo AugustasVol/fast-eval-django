ca87cc0a04315c3a22c5582bcdfb81e28d0b28c1
root <root@661433.s.dedikuoti.lt>
2018-01-23 13:03:28 -0500

    postgresql 	modified:   fasteval/settings.py

--- fasteval/settings.py
+++ fasteval/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'fasteval.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_USER_PASSWD"),
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
